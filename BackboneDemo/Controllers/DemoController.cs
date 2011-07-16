32319588f8fa8a5f7a45b39fd35f94fc91b6a012
Lex Childs <lexchilds@gmail.com>
2011-07-16 08:35:17 -0500

    init

---
+++ BackboneDemo/Controllers/DemoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackboneDemo.Controllers
+{
+    public class DemoController : Controller
+    {
+        //
+        // GET: /Demo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ BackboneDemo/Views/Demo/Index.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/MasterPage.Master" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+
+</asp:Content>
+
+
